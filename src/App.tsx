c298ba1f69bf736cb03c018248d97baa3b0cd4f9
dwnsnu <dwnsnu@snu.ac.kr>
2021-05-29 15:31:14 +0900

    update: cross-env node_path setup and pages with router done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
-import Greetings from "./Greetings";
-import Counter from "./Counter";
-import TestComponent from "./TestComponent";
+import { Route } from "react-router-dom";
+// import About from "About";
+import { Home, About, Service, Study, Contact } from "pages";
+// import Greetings from "components/Greetings";
+// import Counter from "components/Counter";
+// import TestComponent from "components/TestComponent";
 
 function App() {
   const onClick = (name: string) => {
@@ -10,11 +13,18 @@ function App() {
   };
   // return <Greetings name="Hello" onClick={onClick} />;
   return (
-    <React.Fragment>
-      <Greetings name="Hello" onClick={onClick} />
-      <Counter />
-      <TestComponent />
-    </React.Fragment>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/service" component={Service} />
+      <Route path="/study" component={Study} />
+      <Route path="/contact" component={Contact} />
+      {/* <React.Fragment>
+        <Greetings name="Hello" onClick={onClick} />
+        <Counter />
+        <TestComponent />
+      </React.Fragment> */}
+    </div>
   );
 }
 
---
+++ src/pages/study/Study.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Study = () => {
+  return (
+    <div>
+      <h2>Study</h2>
+    </div>
+  );
+};
+
+export default Study;
