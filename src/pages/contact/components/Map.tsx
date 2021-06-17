d3103aa7e15a3cda4b36386c3bc51a1f19c6eaa6
dwnsnu <dwnsnu@snu.ac.kr>
2021-06-18 08:35:46 +0900

    update

--- src/pages/contact/components/Map.tsx
+++ src/pages/contact/components/Map.tsx
@@ -4,7 +4,17 @@ import "./Map.scss";
 function Map() {
   return (
     <div className="map">
-      <h2>Map</h2>
+      <div>img</div>
+      <div>
+        <div className="map-grid">
+          <span>02496 서울특별시 동대문구</span>
+          <span>망우로 12길 1, 7층</span>
+          <span>메일 </span>
+          <span>email@gmail.com</span>
+          <span>전화</span>
+          <span>02-6490-5316</span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/contact/components/News.tsx
+++ src/pages/contact/components/News.tsx
@@ -14,7 +14,7 @@ function News() {
       var newDiv2 = document.createElement("div");
       newDiv2.innerHTML += "글쓰기"
       foundDom.appendChild(newDiv2)
-  
+
       console.log(foundDom)
     }
   }, [])
@@ -26,16 +26,28 @@ function News() {
   // }, []);
   // useEffect(() => {}, []);
   const data = [
-    { id:<div style={{background:"#0A347F", color:"white", lineHeight:"40px", borderRadius:"20px"}}>공지</div>, name: "공지사항 테스트 입니다.", writer: "관리자0", date: "2020.04.10" },
-    { id:1, name: "공지사항 테스트 입니다.", writer: "관리자0", date: "2020.04.09" },
-    { id:2, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자1", date: "2020.04.08" },
-    { id:3, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자1", date: "2020.04.07" },
-    { id:4, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자2", date: "2020.04.06" },
-    { id:5, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자2", date: "2020.04.05" },
-    { id:6, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자3", date: "2020.04.04" },
-    { id:7, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자3", date: "2020.04.03" },
-    { id:8, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.04.02" },
-    { id:9, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.04.01" },
+    { id: <div style={{ background: "#0A347F", color: "white", lineHeight: "40px", borderRadius: "20px" }}>공지</div>, name: "공지사항 테스트 입니다.", writer: "관리자0", date: "2020.04.10" },
+    { id: 1, name: "공지사항 테스트 입니다.", writer: "관리자0", date: "2020.04.09" },
+    { id: 2, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자1", date: "2020.04.08" },
+    { id: 3, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자1", date: "2020.04.07" },
+    { id: 4, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자2", date: "2020.04.06" },
+    { id: 5, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자2", date: "2020.04.05" },
+    { id: 6, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자3", date: "2020.04.04" },
+    { id: 7, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자3", date: "2020.04.03" },
+    { id: 8, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.04.02" },
+    { id: 9, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.04.01" },
+
+    { id: 10, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자2", date: "2020.04.05" },
+    { id: 11, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자3", date: "2020.04.04" },
+    { id: 12, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자3", date: "2020.04.03" },
+    { id: 13, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.04.02" },
+    { id: 14, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.04.01" },
+
+    { id: 15, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.03.09" },
+    { id: 16, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.03.08" },
+    { id: 17, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.03.07" },
+    { id: 18, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.03.06" },
+    { id: 19, name: "[모빌리티 팀] UX 리서치 (신입/경력)", writer: "관리자4", date: "2020.03.05" },
   ];
 
   // Fields to show in the table, and what object properties in the data they bind to
