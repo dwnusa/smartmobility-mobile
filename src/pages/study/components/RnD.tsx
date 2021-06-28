90d6be87d2f6588496de8f33c25afa1c09f7f8dc
dwnsnu <dwnsnu@snu.ac.kr>
2021-06-28 21:40:43 +0900

    click event in rnd

--- src/pages/study/components/RnD.tsx
+++ src/pages/study/components/RnD.tsx
@@ -7,21 +7,28 @@ import { Link, NavLink, useLocation } from "react-router-dom";
 function RnD({ history }: { history: any }) {
   return (
     <div className="rnd">
-      <div className="service-filter">
-      </div>
+      <div className="service-filter"></div>
       <div className="rnd-grid">
-        <img src={rnd01_1}
+        <img
+          src={rnd01_1}
           onClick={() => history.push("/rnd/1")}
-          onMouseOut={e => (e.currentTarget.src = rnd01_1)}
-          onMouseOver={e => (e.currentTarget.src = rnd01_2)} />
+          onMouseOut={(e) => (e.currentTarget.src = rnd01_1)}
+          onMouseOver={(e) => (e.currentTarget.src = rnd01_2)}
+        />
         {/* <Link to="/">
         </Link> */}
-        <img src={rnd02_1}
-          onMouseOut={e => (e.currentTarget.src = rnd02_1)}
-          onMouseOver={e => (e.currentTarget.src = rnd02_2)} />
-        <img src={rnd03_1}
-          onMouseOut={e => (e.currentTarget.src = rnd03_1)}
-          onMouseOver={e => (e.currentTarget.src = rnd03_2)} />
+        <img
+          src={rnd02_1}
+          onClick={() => history.push("/rnd/2")}
+          onMouseOut={(e) => (e.currentTarget.src = rnd02_1)}
+          onMouseOver={(e) => (e.currentTarget.src = rnd02_2)}
+        />
+        <img
+          src={rnd03_1}
+          onClick={() => history.push("/rnd/3")}
+          onMouseOut={(e) => (e.currentTarget.src = rnd03_1)}
+          onMouseOver={(e) => (e.currentTarget.src = rnd03_2)}
+        />
       </div>
     </div>
   );
