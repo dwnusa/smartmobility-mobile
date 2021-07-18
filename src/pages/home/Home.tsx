0aff5330decb536e023d192413154ec5763ca60d
dwnsnu <dwnsnu@snu.ac.kr>
2021-07-18 14:57:19 +0900

    update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import "./Home.scss";
-import { Header } from "components";
+import { Header2 } from "components";
 import { BackgroundVideo } from "./";
 import cardHandycap from "media/card-handycap.png";
 import cardBogota from "media/card-bogota.png";
@@ -48,7 +48,7 @@ function Home() {
   console.log(window.innerWidth)
   return (
     <React.Fragment>
-      <Header isBgShowing={isBgShowing} setBgShowing={setBgShowing} />
+      <Header2 isBgShowing={isBgShowing} setBgShowing={setBgShowing} />
       <BackgroundVideo isShowing={isBgShowing} posX={posX} />
       <div
         className="home"
@@ -57,9 +57,8 @@ function Home() {
       >
         {/* <div className="svgtest" style={{ left: `${posX - 50}vw` }}> */}
         <div
-          className={`svgtest ${
-            !isBgShowing && (nextAnimation ? "activated2" : "activated1")
-          }`}
+          className={`svgtest ${!isBgShowing && (nextAnimation ? "activated2" : "activated1")
+            }`}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -80,7 +79,7 @@ function Home() {
               d="M21.6601 679.12C57.0118 822.071 285.343 987.1 568.659 971.62C894.659 953.808 1044.31 617.62 997.659 510.12C972.272 451.62 904.617 484.521 932.159 548.62C955.362 602.62 1074.16 775.12 1433.66 717.12C1721.22 670.727 2100.79 263.77 2334.79 217.775C2402.62 204.442 2647.85 141.12 2800.66 503.62C2853.49 628.954 2947.66 1022.62 3189.16 1022.62C3444.66 1022.62 3584.67 746.611 3659.66 671.62C3686.66 644.62 3834.66 463.62 4070.16 699.12C4265.73 894.694 4288.16 978.62 4580.16 978.62C4876.66 978.62 5124.16 490.62 4984.16 490.62C4884.84 490.62 5070.66 871.62 5530.66 715.62C5990.66 559.62 5804.16 386.12 6164.66 358.12"
               stroke="#0A347F"
               stroke-width="4"
-              // stroke-dashoffset={offsetValue}
+            // stroke-dashoffset={offsetValue}
             />
           </svg>
         </div>
