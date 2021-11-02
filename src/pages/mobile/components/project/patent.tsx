06ed8ebc355570c2ad986d6c26735b797eb79279
dwnsnu <dwnsnu@snu.ac.kr>
2021-11-02 15:36:30 +0900

    patent

--- src/pages/mobile/components/project/patent.tsx
+++ src/pages/mobile/components/project/patent.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import * as cards from "media";
 import styles from './patent.module.scss';
 
-function Patent2({ishm3Scroll, setPos, pos}) {
+function Patent2({ ishm3Scroll, setPos, pos }) {
   return (
-    <div className={styles["hm3-body-wrapper"]} style={{overflow: ishm3Scroll ? "scroll":"hidden"}}>
+    <div className={styles["hm3-body-wrapper"]} style={{ overflow: ishm3Scroll ? "scroll" : "hidden" }}>
       <div className={styles["hm3-box1"]} >
         {/* <div style={{zIndex:1, border:"1px red solid", position:"absolute", top:"1%", right:"5%", height:"100%", width:"19vw"}}> */}
-          {/* <div onClick={()=>setPos({...pos, stack:0})} style={{zIndex:1, position:"absolute", top:"1%", right:"5%", width:"19vw", height:"19vw", border:"1px red solid"}}></div>
+        {/* <div onClick={()=>setPos({...pos, stack:0})} style={{zIndex:1, position:"absolute", top:"1%", right:"5%", width:"19vw", height:"19vw", border:"1px red solid"}}></div>
           <div onClick={()=>setPos({...pos, stack:1})} style={{zIndex:1, position:"absolute", top:"12%", right:"5%", width:"19vw", height:"19vw", border:"1px red solid"}}></div>
           <div onClick={()=>setPos({...pos, stack:2})} style={{zIndex:1, position:"absolute", top:"24%", right:"5%", width:"19vw", height:"19vw", border:"1px red solid"}}></div>
           <div onClick={()=>setPos({...pos, stack:3})} style={{zIndex:1, position:"absolute", top:"35%", right:"5%", width:"19vw", height:"19vw", border:"1px red solid"}}></div>
@@ -15,21 +15,21 @@ function Patent2({ishm3Scroll, setPos, pos}) {
         {/* </div> */}
         <div>
           <div>
-            <div onClick={()=>setPos({...pos, stack:0})}></div>
-            <div onClick={()=>setPos({...pos, stack:1})}></div>
-            <div onClick={()=>setPos({...pos, stack:2})}></div>
-            <div onClick={()=>setPos({...pos, stack:3})}></div>
-            <div onClick={()=>setPos({...pos, stack:4})}></div>
+            <div onClick={() => setPos({ ...pos, stack: 0 })}></div>
+            <div onClick={() => setPos({ ...pos, stack: 1 })}></div>
+            <div onClick={() => setPos({ ...pos, stack: 2 })}></div>
+            <div onClick={() => setPos({ ...pos, stack: 3 })}></div>
+            <div onClick={() => setPos({ ...pos, stack: 4 })}></div>
           </div>
-          <img src={cards.m_patent1} style={{display:pos.stack === 0 ? "block":"none"}}/>
-          <img src={cards.m_patent2} style={{display:pos.stack === 1 ? "block":"none"}}/>
-          <img src={cards.m_patent3} style={{display:pos.stack === 2 ? "block":"none"}}/>
-          <img src={cards.m_patent4} style={{display:pos.stack === 3 ? "block":"none"}}/>
-          <img src={cards.m_patent5} style={{display:pos.stack === 4 ? "block":"none"}}/>
-        {/* <img src={cards.m_patent2} style={{visibility:pos.stack === 1 ? "visible":"hidden"}}/>
-        <img src={cards.m_patent3} style={{visibility:pos.stack === 2 ? "visible":"hidden"}}/>
-        <img src={cards.m_patent4} style={{visibility:pos.stack === 3 ? "visible":"hidden"}}/>
-        <img src={cards.m_patent5} style={{visibility:pos.stack === 4 ? "visible":"hidden"}}/> */}
+          <img src={cards.m_patent1} style={{ display: pos.stack === 0 ? "block" : "none" }} />
+          <img src={cards.m_patent2} style={{ display: pos.stack === 1 ? "block" : "none" }} />
+          <img src={cards.m_patent3} style={{ display: pos.stack === 2 ? "block" : "none" }} />
+          <img src={cards.m_patent4} style={{ display: pos.stack === 3 ? "block" : "none" }} />
+          <img src={cards.m_patent5} style={{ display: pos.stack === 4 ? "block" : "none" }} />
+          {/* <img src={cards.m_patent2} style={{visibility:pos.stack === 1 ? "visible":"hidden"}}/>
+          <img src={cards.m_patent3} style={{visibility:pos.stack === 2 ? "visible":"hidden"}}/>
+          <img src={cards.m_patent4} style={{visibility:pos.stack === 3 ? "visible":"hidden"}}/>
+          <img src={cards.m_patent5} style={{visibility:pos.stack === 4 ? "visible":"hidden"}}/> */}
         </div>
       </div>
       <div className={styles["hm3-footer"]}>
